9530e931ebbe1762b9a34590ff0d8d2203da0c82
Daniel Palacio <dpalacio@buffalo.edu>
2020-01-15 11:54:23 -0500

    Initial Commit

---
+++ Stilista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stilista
+//
+//  Created by Daniel Palacio on 1/15/20.
+//  Copyright © 2020 Daniel Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
